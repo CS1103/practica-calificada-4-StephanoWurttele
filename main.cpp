c3c1099084d8b6bd62a60fc91df8022b950f6e01
Stephano <stephano.wurttele@utec.edu.pe>
2019-07-05 13:00:55 -0500

    Done

--- main.cpp
+++ main.cpp
@@ -20,11 +20,14 @@ void maximo(vector<A> &vec,int &a,int &b,A &max){
 
 template<typename T>
 T concurrente(vector<T> vec,int numhilos){
+	T max;
+	if(vec.size()==0){
+		return max;
+	}
 	int lim_inf[numhilos];
 	int lim_sup[numhilos];
 	vector<thread> hilos={};
 	T maximos[numhilos];
-	T max;
 	for(int i=0;i<numhilos;i++){
 		lim_inf[i]=(vec.size()*i)/numhilos;
 		lim_sup[i]=(vec.size()*(i+1))/numhilos;
@@ -53,13 +56,19 @@ int main() {
 	int numhilos=4;
 	vector<int> vec={1,2,3,1,3,4,5,1,4,3,2,8,9};
 	int maximo = concurrente(vec,numhilos);
-	cout<<"Main maximo es: "<<maximo<<endl;
+	cout<<"Main maximo1 es: "<<maximo<<endl;
 	vector<float> vec2={13.1,2.13,3.1,1.1,43.1,64.1,53.1,11.1,4.1,3.1,2.1,8.1,9.1};
 	float maximo2 = concurrente(vec2,numhilos);
-	cout<<"Main maximo es: "<<maximo2<<endl;
+	cout<<"Main maximo2 es: "<<maximo2<<endl;
 	vector<double> vec3={15.1,2.1,83.1,1.1,3.1,4.1,5.1,14.1,4.1,3.12,2.19,8.1,93.1};
 	double maximo3 = concurrente(vec3,numhilos);
 	cout<<"Main maximo es: "<<maximo3<<endl;
+	
+	vector<int> vec4={};
+	int maximo4 = concurrente(vec4,numhilos);
+	cout<<"Main maximo es: "<<maximo4<<endl;
+	
+	
 	return 0;
 	
 }
